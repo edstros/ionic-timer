a2811b2c82cda6c3a6adeaa54c76f16bb2910ec2
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-09 14:51:42 -0500

    adding vibrate function

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -5,7 +5,7 @@ angular
     $scope.minutes = '00';
     $scope.seconds = '00';
     // Wait for device API libraries to load
-    //
+
     $scope.vibrateNotify = function () {
       testBtn.addEventListener("click", function () {
         navigator.notification.vibrate(20000);
