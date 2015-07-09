e861f568e70924026a8c2da4006619ab53c2903b
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-07-09 14:48:13 -0500

    add vibrate

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,7 +1,15 @@
 angular
-.module('starter')
-.controller('MainCtrl', ['$scope', function($scope){
-  $scope.hours = '00';
-  $scope.minutes = '00';
-  $scope.seconds = '00';
-}])
+  .module('starter')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.hours = '00';
+    $scope.minutes = '00';
+    $scope.seconds = '00';
+    // Wait for device API libraries to load
+    //
+    $scope.vibrateNotify = function () {
+      testBtn.addEventListener("click", function () {
+        navigator.notification.vibrate(20000);
+        //cordova plugin
+      });
+    }
+}]);
